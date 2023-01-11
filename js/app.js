a2d62c5421caa48c0fe9ce8a30da3431bf48089b
Silje <silje.eidsvoll@gmail.com>
2023-01-11 13:43:29 +0100

    function

--- js/app.js
+++ js/app.js
@@ -7,4 +7,27 @@ age = 33;
 
 console.log(firstname + ' er ikke sliten ' + age); 
 
-console.log('2'+ 4);
+console.log(3 + 4);
+
+function add() {
+    return 3 + 4; 
+}
+
+add(); 
+
+console.log(add()); 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+function multiply(a, b, c, d) {
+    return a * b * c * d;
+}
+
+function divide(a, b) {
+    return a / b; 
+}
+
+console.log(multiply(4, 7, 6, 9));
+console.log(multiply(435, 265, 4, 5));
+
+console.log(divide(10, 21)); 
