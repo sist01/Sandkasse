bea02b3d14b61bcdd6d6d5ed2ed3ac7eaf6cda63
Silje <silje.eidsvoll@gmail.com>
2023-01-18 11:25:08 +0100

    new page - article

--- js/create.js
+++ js/create.js
@@ -9,7 +9,29 @@ const ulElement = document.createElement('ul');
 // Sette nav inni header
 // Variabel som lagrer verdi av html node av header
 const headerElement = document.querySelector('header');
+headerElement.append(navElement);
+
 
 // Sette ul inn i nav
+navElement.append(ulElement);
+
 
 // Sette en etter hverandre alle liater item (li)
+
+
+const li1Element = document.createElement('li');
+li1Element.textContent = 'About';
+ulElement.appendChild(li1Element);
+
+const li2Element = document.createElement('li');
+li2Element.textContent = 'Products';
+ulElement.appendChild(li2Element); 
+
+const li3Elements = document.createElement('li');
+li3Elements.textContent = 'Contact';
+ulElement.appendChild(li3Elements);
+
+
+
+
+console.log(headerElement);
