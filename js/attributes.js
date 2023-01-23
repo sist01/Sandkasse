ca4bb9d49b0e8b96d53b18c6dc6f062b8d12e995
Silje <silje.eidsvoll@gmail.com>
2023-01-23 10:47:13 +0100

    13.01

---
+++ js/attributes.js
@@ -0,0 +1,29 @@
+console.log('attribute');
+
+const h1Element = document.querySelector('h1'); 
+
+h1Element.id = '12876'; // gammel metode
+h1Element.setAttribute('id', 'idbyset'); // beste praksis
+
+console.log(h1Element);
+
+const pElement = document.querySelector('.p-element');
+//pElement.setAttribute('class', 'new-class');
+//pElement.className = 'one-class';
+pElement.setAttribute('style', 'backround-color: #ff0000;')
+console.log(pElement); 
+
+const linkElement = document.createElement('a');
+ 
+
+linkElement.setAttribute('href', 'https://www.kristiania.no');
+linkElement.textContent = 'Link til kristiania'; 
+
+// hente body fra html
+const bodyElement = document.querySelector('body');
+
+// append linkElement til body
+bodyElement.appendChild(linkElement);
+
+console.log(linkElement);
+
