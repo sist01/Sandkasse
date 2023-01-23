7e04d31cf759dae0e711f5af69b176ba92bd6d61
Silje <silje.eidsvoll@gmail.com>
2023-01-23 13:12:09 +0100

    initlist

--- js/list.js
+++ js/list.js
@@ -1,14 +1,36 @@
+console.log('list'); 
+
 function initList() {
+
     // Lage en ul container
+    const ulElement = document.createElement('ul');
 
     // Lage en eller flere list item
+    const liElement = document.createElement('li');
 
     // Setter inn i list item en a tag med href verdi (forskjellige lenker)
+    const aElement = document.createElement('a');
+    aElement.setAttribute('href', 'https://www.google.no');
+    aElement.setAttribute('target', '_blank')
+   
+
+    // Sette inn label for a
+    aElement.textContent = 'Google';
+    liElement.appendChild(aElement);
 
     // Bruk en Loop i alternativ
 
-    // Skrive list item tag inni ul continer
+
+    // Skrive li item tag inni ul continer
+    ulElement.appendChild(liElement);
+
+    console.log(ulElement)
 
     // Sette ul continer med hele liste inn i body
+    const bodyElement = document.querySelector('body');
+    bodyElement.appendChild(ulElement);
 
-}
+    console.log(ulElement)
+
+};
+initList();
