c6ba9c1f534418f20183e928bd64b448b8827eb8
Silje <silje.eidsvoll@gmail.com>
2023-01-23 12:53:35 +0100

    Set up list for doings

---
+++ js/list.js
@@ -0,0 +1,14 @@
+function initList() {
+    // Lage en ul container
+
+    // Lage en eller flere list item
+
+    // Setter inn i list item en a tag med href verdi (forskjellige lenker)
+
+    // Bruk en Loop i alternativ
+
+    // Skrive list item tag inni ul continer
+
+    // Sette ul continer med hele liste inn i body
+
+}
