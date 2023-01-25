34a39bbc4a39169c17c28c428d310aedc95bef88
Silje <silje.eidsvoll@gmail.com>
2023-01-25 09:59:36 +0100

    25.01

---
+++ js/events.js
@@ -0,0 +1,22 @@
+
+
+const averageAge = (a, b, c) => {
+    return (a + b + c) / 3;
+    
+}
+
+console.log(averageAge(12, 18, 24));
+
+
+function calculate(a, b) {
+    return a * b;
+}
+
+console.log(calculate(34, 2));
+
+
+function calculate(value1, value2) {
+    return value1 * value2;
+}
+
+console.log(calculate(34, 2));
