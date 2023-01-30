890b1f1117a8907bc571778a2fd0f8d1340c3480
Silje <silje.eidsvoll@gmail.com>
2023-01-30 11:25:51 +0100

    30.01.23

---
+++ js/data.js
@@ -0,0 +1,23 @@
+const person = [
+    {
+        person: {
+            firstName: 'Silje',
+            lastName: 'Strømstad',
+            age: 21,
+            dob: "2001-16.03",
+            nationality: 'Norway',  
+
+        },
+        department:[ 'student', 'ux/ui'],
+        adress:{
+            state: 'Norway',
+            city: 'Nittedal',
+            street: 'Grindbråtan',
+            housenumber: '60, a',
+            postadress: '1484, Hakadal',
+            mail: 'hei',
+        },
+    }
+]
+
+consol.log(person);
